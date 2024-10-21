f77248a32d5bd178846bf30517b70bf796a60a42
Fabio Gonçalves <fgoncalves@cclindsa.com>
2024-10-21 17:19:14 -0300

    Projeto nlw pocket api

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,11 +1,41 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { createGoal } from '../services/create-goal.js'
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("HTTP server running!");
-  });
+    console.log('HTTP server running!')
+  })
---
+++ server/src/services/create-goal.ts
@@ -0,0 +1,26 @@
+import { db } from '../db/index.js'
+import { goals } from '../db/schema.js'
+
+interface CreateGoalRequest {
+  title: string
+  desiredWeeklyFrequency: number
+}
+
+export async function createGoal({
+  title,
+  desiredWeeklyFrequency,
+}: CreateGoalRequest) {
+  const result = await db
+    .insert(goals)
+    .values({
+      title,
+      desiredWeeklyFrequency,
+    })
+    .returning()
+
+  const goal = result[0]
+
+  return {
+    goal,
+  }
+}
