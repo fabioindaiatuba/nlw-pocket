433fde155a23427a63ffb8f17a871a95355e41c7
Fabio Gonçalves <fgoncalves@cclindsa.com>
2024-10-25 16:18:33 -0300

    Projeto estudo

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,21 @@
-
+import { Plus } from 'lucide-react'
+import letsStart from './assets/lets-start-ilustration.svg'
+import logo from './assets/logo-in-orbit.svg'
+import { Button } from './components/ui/button'
 export function App() {
   return (
-    <h1>Hellow World</h1>
+    <div className="h-screen flex flex-col items-center justify-center gap-8 ">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="in.orbit" />
+      <p className='text-zinc-300 leading-relaxed max-w-80 text-center'>
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+      <Button>
+        <Plus className='size-4' />
+        Cadastrar meta
+      </Button>
+
+    </div>
   )
 }
 
